c3f3f4cfa669b5ad951dd76b1472cfa3efa593f4
MiguelOrtiz <miguel.ort.iq@gmail.com>
2026-06-12 22:45:31 -0600

    Se agrega la funcion del footer

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
 import { navbar } from "./navBar.js";
+import { cargarFooter } from "./footer.js";
 
 
 document.addEventListener("DOMContentLoaded", () =>{
 
     navbar();
+    cargarFooter();
   
 })
