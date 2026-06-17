44295bbf45c32cca2f3427d43a7c12237e74b838
MiguelOrtiz <miguel.ort.iq@gmail.com>
2026-06-17 16:52:33 -0600

    Se importa la funcion readMore

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,15 @@
 import { navbar } from "./navBar.js";
 import { cargarFooter } from "./footer.js";
+import { readMore } from "./projects.js";
+
 
 
 document.addEventListener("DOMContentLoaded", () =>{
 
     navbar();
     cargarFooter();
+    readMore();
+  
+  
   
 })
